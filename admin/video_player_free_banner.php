ed786ce7aadef0b22b873a551b3dc924f7cc648f
Baggi24 <baggi24@mail.ru>
2016-11-28 16:34:46 +0400

    change links

--- admin/video_player_free_banner.php
+++ admin/video_player_free_banner.php
@@ -1,8 +1,8 @@
 <div class="free_version_banner" <?php if( isset($_COOKIE['videoPlayerBannerShow']) && $_COOKIE['videoPlayerBannerShow'] == "no" ) echo 'style="display:none"'; ?> >
     <a class="close_free_banner">+</a>
     <img class="manual_icon" src="<?php echo esc_attr(plugins_url('../images/icon-user-manual.png', __FILE__));?>" alt="user manual" />
-    <p class="usermanual_text">If you have any difficulties in using the options, Follow the link to <a href="http://huge-it.com/wordpress-portfolio-gallery-user-manual/" target="_blank">User Manual</a></p>
-    <a class="get_full_version" href="http://huge-it.com/portfolio-gallery/" target="_blank">GET THE FULL VERSION</a>
+    <p class="usermanual_text">If you have any difficulties in using the options, Follow the link to <a href="http://huge-it.com/wordpress-video-player-user-manual/" target="_blank">User Manual</a></p>
+    <a class="get_full_version" href="http://huge-it.com/video-player/" target="_blank">GET THE FULL VERSION</a>
     <a href="http://huge-it.com" target="_blank"><img class="huge_it_logo" src="<?php echo  esc_attr(plugins_url('../images/Huge-It-logo.png', __FILE__)); ?>"/></a>
     <div style="clear: both;"></div>
     <div class="hg_social_link_buttons">
@@ -12,9 +12,9 @@
         <a target="_blank" class="yt"  href="https://www.youtube.com/channel/UCueCH_ulkgQZhSuc0L5rS5Q"></a>
     </div>
     <div class="hg_view_plugins_block">
-        <a target="_blank"  href="https://wordpress.org/support/plugin/portfolio-gallery/reviews/">Rate Us</a>
-        <a target="_blank"  href="http://huge-it.com/wordpress-plugins-portfolio-gallery-demo/">Full Demo</a>
-        <a target="_blank"  href="http://huge-it.com/wordpress-portfolio-gallery-faq//">FAQ</a>
+        <a target="_blank"  href="https://wordpress.org/support/plugin/video-player/reviews/">Rate Us</a>
+        <a target="_blank"  href="http://huge-it.com/wordpress-plugins-video-player-demo/">Full Demo</a>
+        <a target="_blank"  href="http://huge-it.com/wordpress-video-player-faq/">FAQ</a>
         <a target="_blank"  href="http://huge-it.com/contact-us/">Contact Us</a>
     </div>
     <div  class="description_text"><p>This is the Lite version of the plugin. Click "GET THE FULL VERSION" for more advanced options and customization possibilities. We appreciate your attention and cooperation.</p></div>
@@ -37,7 +37,7 @@
                 <li><a target="_blank" href="https://www.facebook.com/hugeit/" class="facebook"></a></li>
                 <li><a target="_blank" href="https://twitter.com/HugeITcom" class="twitter"></a></li>
                 <li><a target="_blank" href="https://plus.google.com/111845940220835549549" class="google"></a></li>
-                <li><a target="_blank" href="https://www.youtube.com/channel/UCueCH_ulkgQZhSuc0L5rS5Q" class="instagram"></a></li>
+                <li><a target="_blank" href="https://www.youtube.com/channel/UCueCH_ulkgQZhSuc0L5rS5Q" class="youtube"></a></li>
             </ul>
         </div>
     </div>
